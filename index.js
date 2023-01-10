16598856bb8fe2b5ba4e5a0b7cb92b373918cd8c
ZainabYounus <zainabyounus63@gmail.com>
2023-01-10 20:09:03 +0200

    Implemented 3.4: Phonebook backend step4

--- index.js
+++ index.js
@@ -39,13 +39,19 @@ app.get('/info', (request, response) => {
 
 app.get('/api/persons/:id', (request,response) => {
   const id = Number(request.params.id)
-  const person = persons.find(person=> person.id === id) //find returns object. filter returns array.
+  const person = persons.find(person=> person.id === id) //find() returns object.
   if(person){
     response.json(person)
   }
   else{
     response.status(404).end()
   }
+})
+
+app.delete('/api/persons/:id', (request,response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person=> person.id !== id) //filter() returns array.
+  response.status(204).end()
 })
 
 const PORT = 3001
