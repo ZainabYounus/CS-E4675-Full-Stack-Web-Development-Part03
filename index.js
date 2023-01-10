46f01bf2af5e4a3c2442eeadd2d8e2eaca49ce2f
ZainabYounus <zainabyounus63@gmail.com>
2023-01-10 20:35:13 +0200

    Implemented 3.6: Phonebook backend step6

--- index.js
+++ index.js
@@ -25,19 +25,23 @@ let persons = [
     }
 ]
 
+// Root page
 app.get('/', (request,response) => {
     response.send('<h1>Hello World </h1>')
 })
 
+// GET ALL
 app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
+// INFO Page
 app.get('/info', (request, response) => {
   const date = new Date().toString();
   response.send(`<p>Phonebook has info for ${persons.length} people</p> <p>${date}</p>`)
 })
 
+// GET BY ID
 app.get('/api/persons/:id', (request,response) => {
   const id = Number(request.params.id)
   const person = persons.find(person=> person.id === id) //find() returns object.
@@ -49,17 +53,39 @@ app.get('/api/persons/:id', (request,response) => {
   }
 })
 
+// DELETE
 app.delete('/api/persons/:id', (request,response) => {
   const id = Number(request.params.id)
   persons = persons.filter(person=> person.id !== id) //filter() returns array.
   response.status(204).end()
 })
 
+// CREATE
 app.post('/api/persons', (request, response) => {
   let newId = getRandomInt(100000);
-  const newNote = request.body
-  newNote.id = newId
-  persons = persons.concat(newNote)
+  const newPerson = request.body
+
+  if(!newPerson.name){
+    return response.status(400).json({
+      error: 'name is missing'
+    })
+  }
+
+  else if(!newPerson.number){
+    return response.status(400).json({
+      error: 'number is missing'
+    })
+  }
+
+  const person = persons.find(person => person.name === newPerson.name)
+  if(person){
+    return response.status(400).json({
+      error: 'name must be unique'
+    })
+  }
+
+  newPerson.id = newId
+  persons = persons.concat(newPerson)
   response.json(persons)
 })
 
