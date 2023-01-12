e571ce09b0ad45d81f26d50532418d46e423d64a
ZainabYounus <zainabyounus63@gmail.com>
2023-01-12 19:51:33 +0200

    Implemented 3.9 - 3.11

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
 var morgan = require('morgan')
+const cors = require('cors')
 
 app.use(express.json())
 app.use(morgan('tiny'))
+app.use(cors())
+app.use(express.static('build'))
 
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 
@@ -101,7 +104,7 @@ function getRandomInt(max) {
 }
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
